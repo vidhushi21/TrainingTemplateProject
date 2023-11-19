5b0bc5cd22a5ab0503f035e7632a82e49b3df6a1
vidhushi21 <vkondaba@gmail.com>
2023-11-19 17:17:21 -0500

    Update DriveSubsystem.java

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -7,12 +7,55 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+
+
+
 public class DriveSubsystem extends SubsystemBase {
 
+  private PWMSparkMax m_leftMotor = new PWMSparkMax(0);
+  private PWMSparkMax m_rightMotor = new PWMSparkMax(1);
+  // Create the simulation model of our drivetrain.
+  DifferentialDrivetrainSim m_driveSim = new DifferentialDrivetrainSim(
+  DCMotor.getNEO(2),       // 2 NEO motors on each side of the drivetrain.
+  7.29,                    // 7.29:1 gearing reduction.
+  7.5,                     // MOI of 7.5 kg m^2 (from CAD model).
+  60.0,                    // The mass of the robot is 60 kg.
+  Units.inchesToMeters(3), // The robot uses 3" radius wheels.
+  0.7112,                  // The track width is 0.7112 meters.
+
+  // The standard deviations for measurement noise:
+  // x and y:          0.001 m
+  // heading:          0.001 rad
+  // l and r velocity: 0.1   m/s
+  // l and r position: 0.005 m
+  VecBuilder.fill(0.001, 0.001, 0.001, 0.1, 0.1, 0.005, 0.005));
+
   public DriveSubsystem() {}
+
+  public Drivetrain() {
+    m_leftEncoder.setDistancePerPulse(2 * Math.PI * kWheelRadius / kEncoderResolution);
+    m_rightEncoder.setDistancePerPulse(2 * Math.PI * kWheelRadius / kEncoderResolution);
+    }
 
   @Override
   public void simulationPeriodic() {
     // This method will be called once per scheduler run during simulation
+          // Set the inputs to the system. Note that we need to convert
+      // the [-1, 1] PWM signal to voltage by multiplying it by the
+      // robot controller voltage.
+      m_driveSim.setInputs(m_leftMotor.get() * RobotController.getInputVoltage(),
+      m_rightMotor.get() * RobotController.getInputVoltage());
+
+    // Advance the model by 20 ms. Note that if you are running this
+    // subsystem in a separate thread or have changed the nominal timestep
+    // of TimedRobot, this value needs to match it.
+    m_driveSim.update(0.02);
+
+    // Update all of our sensors.
+    m_leftEncoderSim.setDistance(m_driveSim.getLeftPositionMeters());
+    m_leftEncoderSim.setRate(m_driveSim.getLeftVelocityMetersPerSecond());
+    m_rightEncoderSim.setDistance(m_driveSim.getRightPositionMeters());
+    m_rightEncoderSim.setRate(m_driveSim.getRightVelocityMetersPerSecond());
+    m_gyroSim.setAngle(-m_driveSim.getHeading().getDegrees());
   }
 }
